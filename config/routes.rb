2c5195730e534fa03971d6eb5766beb560cfb85d
github username <github email address>
2019-06-28 14:07:32 -0600

    Updated the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :buddy_infos
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root  'buddy_infos#index'
+  resources :buddy_infos
 end
