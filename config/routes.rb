a5c5b73a52ff96f3c773a48f008d004505d1591c
github username <github email address>
2019-06-28 13:54:43 -0600

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :buddy_infos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root  'buddy_infos#index'
 end
